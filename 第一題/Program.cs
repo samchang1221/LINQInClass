0c88dab1992a38a4cbb74914638ce199ea9a2ce3
samchang1221 <samchang1221@outlook.com>
2023-02-21 11:18:07 +0800

    加入專案檔案。

---
+++ 第一題/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace 第一題
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some numbers to order descending:(ex:1,2,3,4,5,6)");
+            string read = Console.ReadLine();
+            var rd = read.Split(',');
+            List<MyData> list = new List<MyData>();
+            foreach (var item in rd)
+            {
+                list.AddRange(CreateList(item));
+            }
+            var order = list.Select(x=>x.str);
+            var reverse_order = order.Reverse();
+            Console.WriteLine(String.Join(",", reverse_order));
+            Console.ReadKey();
+        }
+        static List<MyData> CreateList(string rd)
+        {
+            return new List<MyData>()
+            {
+                new MyData{ str =rd },
+            };
+        }
+        class MyData
+        {
+            public string str { get; set; }
+        }
+    }
+}
---
+++ 第二題/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第二題
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some numbers to sort odd or even:(ex:1,2,3,4,5,6)");
+            string read = Console.ReadLine();
+            var rd = read.Split(',');
+            List<MyData> list = new List<MyData>();
+            foreach (var item in rd)
+            {
+                list.AddRange(CreateList(item));
+            }
+            var odd = list.Where(x => x.num % 2 == 1).Select(x => x.num);
+            var even = list.Where(x => x.num % 2 == 0).Select(x => x.num);
+            Console.WriteLine($"odd {String.Join(",", odd)}");
+            Console.WriteLine($"even {String.Join(",", even)}");
+            Console.ReadKey();
+        }
+        static List<MyData> CreateList(string rd)
+        {
+            return new List<MyData>()
+            {
+                new MyData{ num = Convert.ToInt32(rd) },
+            };
+        }
+        class MyData
+        {
+            public int num { get; set; }
+        }
+    }
+}
